82198e1000c3fcf76f7951c14615d8e356f9ea23
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-13 21:05:47 +0100

    adds association table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
   has_many :subscriptions, foreign_key: :follower_id, dependent: :destroy
 
   has_many :leaders, through: :subscriptions
+
+  has_many :reverse_subscriptions, foreign_key: :leader_id, dependent: :destroy, class_name: "Subscription"
+
+  has_many :followers, through: :reverse_subscriptions
+  def following?(leader)
+    leaders.include? leader
+  end
+
+  def follow!(leader)
+    if leader != self && !following?(leader)
+      leaders << leader
+    end
+  end
 end
